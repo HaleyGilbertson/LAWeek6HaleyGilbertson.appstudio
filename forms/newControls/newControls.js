a4d34104fdc6e84aaa472f69b9414f8f086b2790
HaleyGilbertson <hag79621@creighton.edu>
2021-10-01 12:03:54 -0500

    newControls

---
+++ forms/newControls/newControls.js
@@ -0,0 +1,19 @@
+
+Hamburger1.onclick=Function(){
+  console.log(`You have clicked the hamburger control!`)
+}
+
+
+Dropdown1.onclick=Function(){
+  console.log(`You have clicked the drop down!`)
+}
+
+
+Switch1.onchange=Function(){
+  console.log(`You have switched the button!`)
+}
+
+
+Pagination1.onclick=Function(){
+  console.log(`You have switched pages!`)
+}
